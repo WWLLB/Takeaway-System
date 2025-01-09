364e95085d9683a466985c1fec368046164136fa
WWLLB <836163719@qq.com>
2025-01-09 13:31:52 +0800

    菜品分页查询功能代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,6 +1,8 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -14,4 +16,11 @@ public interface DishService {
     public void savewithlavor(DishDTO dishDTO);
 
 
+    /**
+     *
+     * 菜品分页查询方法
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
 }
